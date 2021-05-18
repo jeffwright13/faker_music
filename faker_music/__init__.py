3fcea5e09e6055dd25af0afd1d95e858f37fbcb8
Jeff Wright <jeff.washcloth@gmail.com>
2021-05-18 17:42:43 -0400

    Fleshing out standard directory/file structure.

---
+++ faker_music/__init__.py
@@ -0,0 +1,5 @@
+"""Music Faker Community Provider"""
+
+__version__ = "0.1"
+
+from .airports import AirTravelProvider
---
+++ faker_music/music.py
@@ -0,0 +1,11 @@
+from faker.providers import BaseProvider
+from random import choice, choices, randint
+
+
+class MusicProvider(BaseProvider):
+   """
+   A Provider for music-related data.
+   """
+
+   def music_instrument(self):
+      pass
