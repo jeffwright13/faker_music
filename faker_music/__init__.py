e88b995302057442e38d74b3ab10b18b443de7bb
Jeff Wright <jeff.washcloth@gmail.com>
2021-05-18 21:59:35 -0400

    Added genre functions.

--- faker_music/__init__.py
+++ faker_music/__init__.py
@@ -1,3 +1,5 @@
 """Music Faker Community Provider"""
 
 __version__ = "0.1"
+
+from .music import MusicProvider
--- faker_music/music.py
+++ faker_music/music.py
@@ -1,11 +1,21 @@
 from faker.providers import BaseProvider
 from random import choice, choices, randint
-
+from genres import genre_list
 
 class MusicProvider(BaseProvider):
-   """
-   A Provider for music-related data.
-   """
+    """
+    A Provider for music-related data.
 
-   def music_instrument(self):
-      pass
+    >>> from faker import Faker
+    >>> from faker_music import MusicProvider
+    >>> fake = Faker()
+    >>> fake.add_provider(MusicProvider)
+    """
+
+    def genre_object(self):
+        g = choice(genre_list)
+        return g
+
+    def genre_name(self):
+        g = choice(genre_list)
+        return g["genre"]
