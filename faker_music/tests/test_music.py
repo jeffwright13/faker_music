15da77a943a025be5403b6eae8fa34808e82454f
Jeff Wright <jeff.washcloth@gmail.com>
2021-05-25 04:10:18 -0400

    Moved faker instantiation to setup in conftest.

--- faker_music/tests/test_music.py
+++ faker_music/tests/test_music.py
@@ -1,43 +1,36 @@
-from faker import Faker
-from faker_music import MusicProvider
-
-fake = Faker()
-fake.add_provider(MusicProvider)
-
-
-def test_music_genre_object():
+def test_music_genre_object(fake):
     test_genre_obj = fake.music_genre_object()
     assert isinstance(test_genre_obj, dict)
     assert "genre" in test_genre_obj.keys()
     assert "subgenres" in test_genre_obj.keys()
 
 
-def test_music_genre():
+def test_music_genre(fake):
     test_genre = fake.music_genre()
     assert isinstance(test_genre, str)
     assert len(test_genre) > 0
 
 
-def test_music_subgenre():
+def test_music_subgenre(fake):
     test_subgenre = fake.music_subgenre()
     assert isinstance(test_subgenre, str)
     assert len(test_subgenre) > 0
 
 
-def test_music_instrument_object():
+def test_music_instrument_object(fake):
     test_instrument_obj = fake.music_instrument_object()
     assert isinstance(test_instrument_obj, dict)
     assert "category" in test_instrument_obj.keys()
     assert "instruments" in test_instrument_obj.keys()
 
 
-def test_music_instrument():
+def test_music_instrument(fake):
     test_instrument = fake.music_instrument()
     assert isinstance(test_instrument, str)
     assert len(test_instrument) > 0
 
 
-def test_music_instrument_category():
+def test_music_instrument_category(fake):
     test_instrument_category = fake.music_instrument_category()
     assert isinstance(test_instrument_category, str)
     assert len(test_instrument_category) > 0
